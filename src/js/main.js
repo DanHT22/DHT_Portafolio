6171dd0cfc8f88037deab3e021cacb0fb60156e3
DanHT22 <tolumes.daniela@gmail.com>
2023-08-28 02:51:28 -0600

    se reajusta portafolio

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,24 @@
-// ******** ANIMACIÓN DEL TÍTULO ***********
-const typed = new Typed (".typed", {
-    strings: [
-        '<h1 class="desarrolladora text-light">FRONTEND JUNIOR</h1>' 
-    ],
-    //stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
-	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
-	//startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
-	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
-	//smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
-	//shuffle: false, // Alterar el orden en el que escribe las palabras.
-	backDelay: 1000, // Tiempo de espera despues de que termina de escribir una palabra.
-	loop: true, // Repetir el array de strings
-	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
-	contentType: 'html', // 'html' o 'null' para texto sin formato
+// SCROLL NAV BAR
+window.onscroll = function() {
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0 ) {
+        document.getElementById('navbar').classList.add('scrolled');
+    } else {
+        document.getElementById('navbar').classList.remove('scrolled');
+    }
+}
+
+// AOS
+AOS.init({
+    duration: 800,
 });
+
+
+// ANIMACIÓN TEXTO 
+const typed = new Typed('.typed', {
+    strings: ['DESARROLLADORA WEB', 'FRONTEND JUNIOR'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+});
+
