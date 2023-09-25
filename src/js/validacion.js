5b227da3e17780d7ae8b7a30191bee16f9ad88e0
DanHT22 <tolumes.daniela@gmail.com>
2023-09-25 15:02:36 -0600

    ajuste de links

--- src/js/validacion.js
+++ src/js/validacion.js
@@ -1,5 +1,5 @@
 // ******* VALIDACIÓN DEL FORMULARIO ******
-
+var Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } };
 // Traer los elementos
 let btnEnviar = document.getElementById("btnEnviar");
 let btnReset = document.getElementById("btnReset");
@@ -118,13 +118,20 @@ btnEnviar.addEventListener("click", function (e) {
     }
 
     if (nombreVal == true && apellidoVal == true && emailVal == true && mensajeVal == true) {
+        Email.send({
+            SecureToken: '4397aba7-2e30-4120-9b80-c7fe47087def',
+            To: "tolumes.daniela@gmail.com",
+            From: txtEmail.value,
+            Subject: "Hola estimado",
+            Body: "Este es un mensaje de prueba"
+        }).then(
         Swal.fire({
             position: 'center',
             icon: 'success',
             title: 'Tu Mensaje ha sido enviado correctamente',
             showConfirmButton: false,
             timer: 1500
-        })
+        })  );
         txtNombre.value = "";
         txtApellido.value = "";
         txtEmail.value = "";
